baaf760255cbdd80bc32bbef8a932c63c906a35c
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-11-29 07:49:32 +0500

    Task7

---
+++ Tyuiu.BaldinAA.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Tyuiu.BaldinAA.Sprint5.Task7.V23.Lib;
+namespace Tyuiu.BaldinAA.Sprint5.Task7.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
+            string pathSaveFile = @"C:\Users\Artyom\source\repos\Tyuiu.BaldinAA.Sprint5\Tyuiu.BaldinAA.Sprint5.Task7.V23\bin\Debug\net8.0\OutPutFileTask7.V23.txt";
+
+
+            Console.WriteLine("Данные находяться в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле : ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey(); ;
+        }
+    }
+}
